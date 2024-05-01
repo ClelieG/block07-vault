e882c4543a9f8dbe754bbda722915a6ce3aa1463
ClelieG <clelieolivier@aol.com>
2024-05-01 18:43:41 -0500

    homework

---
+++ scriptfile.js
@@ -0,0 +1,16 @@
+// Secure the Vault: create variables that result in three calculations that create a combination to a vault 
+
+
+const str1 = " You have received this message because you have been chosen to open an important vault. ";
+const str2 = "Here is the secret combination: " + comb1-comb2-comb3 + "";
+const str3Complete = ` ALERT ${str1} ${str2} ! `;
+
+console.log(str3Complete);
+/* expected: ALERT You have received this message because you have been chosen to open an important vault. Here is the secrent combination: 10-40-39 - SHARE WITH NO ONE! */
+
+alert(str3Complete);
+
+let  comb1 = 3650 / 365;
+let comb2 = 34 + 6;
+let comb3 = 61 - 22;
+/*VAULT COMBINATIONS*/
